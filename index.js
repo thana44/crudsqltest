a5f6b393fac2558c29c38c18177738e787f734aa
thana44 <95677361+thana44@users.noreply.github.com>
2024-07-04 23:11:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -147,7 +147,8 @@ app.post('/login', async(req, res)=>{
                 if(err) return res.json(err)
                 if(match){
                    const token =  jwt.sign({email: data[0].email}, 'my-secrect',{expiresIn: '1h'})
-                   return res.cookie('mytoken', token,{httpOnly: true,sameSite:'None',secure:true}).json({status: 'login success.'})
+                    const hours = new Date(Date.now() + 3600000)
+                   return res.cookie('mytoken', token,{httpOnly: true,sameSite:'None',secure:true,expires:hours}).json({status: 'login success.'})
                 }
                 return res.json('password is wrong.')
             })
